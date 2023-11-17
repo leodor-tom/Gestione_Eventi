075c85a6e91ebe32809c88e495ab04dd602d01f6
leodor-tom <eleodoritom@gmail.com>
2023-11-17 11:16:59 +0100

    Create JWTTools.java

---
+++ src/main/java/TommasoEleodori/Gestione_Eventi/security/JWTTools.java
@@ -0,0 +1,37 @@
+package TommasoEleodori.Gestione_Eventi.security;
+
+import TommasoEleodori.Gestione_Eventi.exceptions.UnauthorizedException;
+import TommasoEleodori.Gestione_Eventi.users.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTTools {
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
+    public String createToken(User user) {
+        return Jwts.builder().setSubject(String.valueOf(user.getId()))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes())).compact();
+    }
+
+    public void verifyToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build().parse(token);
+        } catch (Exception e) {
+            throw new UnauthorizedException("The token it's not valid! Please try again");
+        }
+    }
+
+    public String extractSubject(String token) {
+        return Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build().parseClaimsJws(token).getBody().getSubject();
+    }
+}
