0ff493c209ee363aaba4c7766b29bed2cf3a0b13
leodor-tom <eleodoritom@gmail.com>
2023-11-17 09:50:29 +0100

    Initial commit

---
+++ src/test/java/TommasoEleodori/Gestione_Eventi/GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package TommasoEleodori.Gestione_Eventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
