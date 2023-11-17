3a7a4f103d9cd897b784cc47e8412a13ad93edda
leodor-tom <eleodoritom@gmail.com>
2023-11-17 10:18:52 +0100

    UnauthorizeException + ExceptionHandler

---
+++ src/main/java/TommasoEleodori/Gestione_Eventi/exceptions/ExceptionsHandler.java
@@ -0,0 +1,44 @@
+package TommasoEleodori.Gestione_Eventi.exceptions;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestControllerAdvice
+public class ExceptionsHandler {
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponseDTO handleNotFound(NotFoundException e) {
+        return new ErrorResponseDTO(e.getMessage(), new Date());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorsResponseListDTO handleBadRequest(BadRequestException e) {
+        if (e.getErrorList() != null) {
+            List<String> errorsList = e.getErrorList().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .toList();
+            return new ErrorsResponseListDTO(e.getMessage(), new Date(), new ArrayList<>());
+        }else {
+            return new ErrorsResponseListDTO(e.getMessage(), new Date(), new ArrayList<>());
+        }
+    }
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponseDTO handleAccessDenied(AccessDeniedException e){return  new ErrorResponseDTO(e.getMessage(), new Date());}
+
+    @ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponseDTO handleUnauthorized(UnauthorizedException e){return new ErrorResponseDTO(e.getMessage(), new Date());}
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponseDTO handleGenericError(Exception e){ return new ErrorResponseDTO("Our bad! we will provide as soon as possible", new Date())}
+}
---
+++ src/main/java/TommasoEleodori/Gestione_Eventi/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package TommasoEleodori.Gestione_Eventi.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
